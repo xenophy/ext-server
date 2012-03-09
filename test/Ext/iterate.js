627b27f5ce0ce9f8a565c972e06d8d00f38e08a7
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2012-03-10 07:32:27 +0900

    Ext.JSON追加、Ext.Array追加中

---
+++ test/Ext/iterate.js
@@ -0,0 +1,52 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * Ext Server
+ * Copyright (c)2012 Xenophy.CO.,LTD All rights Reserved.
+ * MIT Licensed
+ */
+
+// {{{ requires
+
+require('../../index.js');
+
+// }}}
+// {{{ describe
+
+describe('Ext.iterate', function() {
+
+    /*
+    it('オブジェクトを繰り返して値をコピーできること', function() {
+
+        var dest = {},
+            src = {
+                key1: 'value1',
+                key2: 'value2',
+                key3: 'value3',
+                key4: 'value4',
+                key5: 'value5'
+            };
+
+        Ext.iterate(src, function(key, v, o) {
+            dest[key] = v;
+        });
+
+        assert.equal(dest.key1, 'value1', 'Test key=key1 value=value1');
+        assert.equal(dest.key2, 'value2', 'Test key=key2 value=value2');
+        assert.equal(dest.key3, 'value3', 'Test key=key3 value=value3');
+        assert.equal(dest.key4, 'value4', 'Test key=key4 value=value4');
+        assert.equal(dest.key5, 'value5', 'Test key=key5 value=value5');
+    });
+
+        */
+});
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
