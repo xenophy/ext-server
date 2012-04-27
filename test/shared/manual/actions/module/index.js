3c870f0792430c661420c49d1d65f55a210bb59f
Kazuhiro Kotsutsumi <kotsutsumi@gmail.com>
2012-04-27 11:07:12 +0900

    action object style support

---
+++ test/shared/manual/actions/module/index.js
@@ -0,0 +1,27 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * Ext Server
+ * Copyright (c)2012 Xenophy.CO.,LTD All rights Reserved.
+ * MIT Licensed
+ */
+
+// {{{ index.js
+
+module.exports = {
+
+    execute: function() {
+
+    }
+
+};
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
---
+++ test/shared/manual/actions/patterns/d.js
@@ -0,0 +1,25 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * Ext Server
+ * Copyright (c)2012 Xenophy.CO.,LTD All rights Reserved.
+ * MIT Licensed
+ */
+
+// {{{ d.js
+
+module.exports = {
+    execute: function() {
+        console.log("d-1");
+    }
+};
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
