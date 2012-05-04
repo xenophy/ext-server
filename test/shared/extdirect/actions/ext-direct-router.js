b6f4733c813886bf0adad4107e1249a4db9bf10f
Kazuhiro Kotsutsumi <kotsutsumi@gmail.com>
2012-05-05 06:03:26 +0900

    implemented, response remoting api, auto parse module in action.

---
+++ test/shared/extdirect/actions/ext-direct-router.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+
+    directrouter: true
+
+};
+
+/*
+    function() {
+
+    var res = this.res;
+
+    res.writeHead(200);
+    res.header('Content-Type', 'application/javascript');
+    res.end("alert(123);");
+
+};
+*/
