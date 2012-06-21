d95611fc1850c6033ff2db333e2aef9bbcc62e7e
NAKAMURA, Hisashi <hisashi@sunvisor.net>
2012-06-21 18:08:03 +0900

    update documents

--- test/database/MySQL/index.js
+++ test/database/MySQL/index.js
@@ -15,6 +15,32 @@ require('../../../index.js');
 
 describe('Ext.database.MySQL', function() {
 
+    var db, table;
+
+    beforeEach(function() {
+        db = Ext.create('Ext.database.MySQL', {
+            // config
+            database: 'extserver',
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            port: '3306'
+        });
+        table = 'users';
+
+    });
+
+    it("find id=1 がkotsutsumiであること", function(){
+        var where = { id: 1 };
+
+        db.connect(function(){
+            assert.equal('a', 'b');
+            db.find(table, where, function(ret){
+                ret.name.should.equal('kotsutsumi');
+                db.end();
+            });
+        });
+    });
 
 });
 
